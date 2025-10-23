6263b485689c1045bbe4d51e40768b636375fa84
Valentina <vperalta22540@gmail.com>
2025-10-23 09:26:24 -0300

    documentación

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pygame
 
+# inicializa la pantalla
 pygame.display.init()
 
+#configura la pantalla
 pygame.display.set_mode(size=(500, 500), flags=0, display=0, vsync=0)
 pygame.display.set_caption("Auxilio")
 
+#variable True para bucle
 run = True
 
+#mientras que "run" sea true...
 while run:
+    # bucle que se ejecuta cada vez que el usuario hace una accion (event)
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:  # Check if the user closed the window
+        # si dicho event es QUIT (cerrar la ventana), run es false (se deja de ejecutar el bucle)
+        if event.type == pygame.QUIT:
             run = False
+    
+    #se actualiza la ventana automáticamente siempre que corra el ciclo
     pygame.display.update()
 
+#se cierra pygame una vez finalizado el ciclo de run
 pygame.quit()
