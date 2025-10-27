dddf1427fdbee5391bb0945dfeb2b33ae3124fa4
Valentina <vperalta22540@gmail.com>
2025-10-27 12:57:13 -0300

    assets bases de torta

--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import pygame
 
-# inicializa la pantalla
-pygame.display.init()
+def start_game():
+    # inicializa la pantalla
+    pygame.display.init()
+    screen_width = 735
+    screen_height = 490
 
-# dimensiones de la pantalla
-screen_width = 735
-screen_height = 490
+    #configura e inicializa la pantalla
+    screen = pygame.display.set_mode(size=(screen_width, screen_height))
+    pygame.display.set_caption("Make your cake")
 
-#configura e inicializa la pantalla
-screen = pygame.display.set_mode(size=(screen_width, screen_height))
-pygame.display.set_caption("Auxilio")
+    #cargar imagen de fondo
+    background = pygame.image.load('assets/bg.jpg').convert()
+    background = pygame.transform.scale( background, (screen_width, screen_height))
 
-#imagen de fondo
-background = pygame.image.load('assets/bg.jpg').convert()
-background = pygame.transform.scale( background, (screen_width, screen_height))
+    #cargar imagen de mesa modo png y posicionar
+    table = pygame.image.load('assets/table.png').convert_alpha()
+    tamaño_mesa = (600, 600)
+    table = pygame.transform.scale(table, tamaño_mesa)
+    table_rect = table.get_rect()
+    table_rect.center = ((screen_width // 2) - 150, (screen_height // 2) + 220)
 
-#bucle para mantener la ventana abierta
-run = True
+    #cargar base png y posicionar
+    base = pygame.image.load('assets/bases/base-vainilla.png').convert_alpha()
+    base_size = (350, 350)
+    base = pygame.transform.scale(base, base_size)
+    base_rect = base.get_rect()
+    base_rect.center = ((screen_width // 2) - 150, (screen_height // 2))
 
-while run:
-    # bucle que se ejecuta cada vez que el usuario hace una accion (event)
-    for event in pygame.event.get():
-        # si dicho event es QUIT (cerrar la ventana), run es false (se deja de ejecutar el bucle)
-        if event.type == pygame.QUIT:
-            run = False
+    #bucle para mantener la ventana abierta
+    run = True
+    while run:
+        # bucle que se ejecuta cada vez que el usuario hace una accion (event)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    screen.blit(background, (0,0))
+        # Copiar imágenes a coords
+        screen.blit(background, (0,0))
+        screen.blit(table, table_rect)
+        screen.blit(base, base_rect)
 
-    pygame.display.flip()
+        # actualiza la ventana completa
+        pygame.display.flip()
 
-#se cierra pygame una vez finalizado el ciclo de run
+start_game()
 pygame.quit()
